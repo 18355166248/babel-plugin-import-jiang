18183840c238e2027d049ea35ba6a33cfccc36fd
‘江浪’ <643546122@qq.com>
2023-03-17 00:09:05 +0800

    feat: 补充2

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,5 +1,14 @@
 export default class Plugin {
-  constructor(libraryName, libraryDirectory, style) {
+  libraryName: string;
+  libraryDirectory: string;
+  style: boolean;
+  pluginStateKey: string;
+  constructor(libraryName, libraryDirectory, style, index = 0) {
     console.log(libraryName, libraryDirectory, style);
+    this.libraryName = libraryName;
+    this.libraryDirectory =
+      typeof libraryDirectory === "undefined" ? "lib" : libraryDirectory;
+    this.style = style || false;
+    this.pluginStateKey = `pluginStateKey${index}`;
   }
 }
